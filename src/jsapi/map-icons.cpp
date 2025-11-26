0f1f201e8fb29beb7dc588092948b1b739ae02e1
iAmir <hhm6@yahoo.com>
2025-11-26 18:37:48 +0330

    add map icon js api

--- src/jsapi/map-icons.cpp
+++ src/jsapi/map-icons.cpp
@@ -19,57 +19,60 @@
 #include "../natives.h"
 #include "../core.h"
 #include "../utility.h"
+#include <omp-node.hpp>
 
-cell AMX_NATIVE_CALL Natives::CreateDynamicMapIcon(AMX *amx, cell *params)
+OMPNODE_API(StreamerMapIcon, Create, float x, float y, float z, int type, int color, int worldid, int interiorid, int playerid, float streamdistance, int style, int areaid, int priority)
 {
-	CHECK_PARAMS(12);
 	if (core->getData()->getGlobalMaxItems(STREAMER_TYPE_MAP_ICON) == core->getData()->mapIcons.size())
 	{
 		return INVALID_STREAMER_ID;
 	}
 	int mapIconId = Item::MapIcon::identifier.get();
 	Item::SharedMapIcon mapIcon(new Item::MapIcon);
-	mapIcon->amx = amx;
+	mapIcon->amx = nullptr; // TODO must be checked if it's used anywhere
 	mapIcon->mapIconId = mapIconId;
 	mapIcon->inverseAreaChecking = false;
 	mapIcon->originalComparableStreamDistance = -1.0f;
 	mapIcon->positionOffset = Eigen::Vector3f::Zero();
 	mapIcon->streamCallbacks = false;
-	mapIcon->position = Eigen::Vector3f(amx_ctof(params[1]), amx_ctof(params[2]), amx_ctof(params[3]));
-	mapIcon->type = static_cast<int>(params[4]);
-	mapIcon->color = static_cast<int>(params[5]);
-	Utility::addToContainer(mapIcon->worlds, static_cast<int>(params[6]));
-	Utility::addToContainer(mapIcon->interiors, static_cast<int>(params[7]));
-	Utility::addToContainer(mapIcon->players, static_cast<int>(params[8]));
-	mapIcon->comparableStreamDistance = amx_ctof(params[9]) < STREAMER_STATIC_DISTANCE_CUTOFF ? amx_ctof(params[9]) : amx_ctof(params[9]) * amx_ctof(params[9]);
-	mapIcon->streamDistance = amx_ctof(params[9]);
-	mapIcon->style = static_cast<int>(params[10]);
-	Utility::addToContainer(mapIcon->areas, static_cast<int>(params[11]));
-	mapIcon->priority = static_cast<int>(params[12]);
+	mapIcon->position = Eigen::Vector3f(x, y, z);
+	mapIcon->type = type;
+	mapIcon->color = color;
+	Utility::addToContainer(mapIcon->worlds, worldid);
+	Utility::addToContainer(mapIcon->interiors, interiorid);
+	Utility::addToContainer(mapIcon->players, playerid);
+	mapIcon->comparableStreamDistance = streamdistance < STREAMER_STATIC_DISTANCE_CUTOFF ? streamdistance : streamdistance * streamdistance;
+	mapIcon->streamDistance = streamdistance;
+	mapIcon->style = style;
+	Utility::addToContainer(mapIcon->areas, areaid);
+	mapIcon->priority = priority;
 	core->getGrid()->addMapIcon(mapIcon);
 	core->getData()->mapIcons.insert(std::make_pair(mapIconId, mapIcon));
-	return static_cast<cell>(mapIconId);
+
+	int ret = mapIconId;
+	API_RETURN(int ret);
 }
 
-cell AMX_NATIVE_CALL Natives::DestroyDynamicMapIcon(AMX *amx, cell *params)
+OMPNODE_API(StreamerMapIcon, Destroy, int mapiconid)
 {
-	CHECK_PARAMS(1);
-	std::unordered_map<int, Item::SharedMapIcon>::iterator m = core->getData()->mapIcons.find(static_cast<int>(params[1]));
+	bool ret = false;
+	std::unordered_map<int, Item::SharedMapIcon>::iterator m = core->getData()->mapIcons.find(mapiconid);
 	if (m != core->getData()->mapIcons.end())
 	{
 		Utility::destroyMapIcon(m);
-		return 1;
+		ret = true;
 	}
-	return 0;
+	ret = false;
+	API_RETURN(bool ret);
 }
 
-cell AMX_NATIVE_CALL Natives::IsValidDynamicMapIcon(AMX *amx, cell *params)
+OMPNODE_API(StreamerMapIcon, IsValid, int mapiconid)
 {
-	CHECK_PARAMS(1);
-	std::unordered_map<int, Item::SharedMapIcon>::iterator m = core->getData()->mapIcons.find(static_cast<int>(params[1]));
+	bool ret = false;
+	std::unordered_map<int, Item::SharedMapIcon>::iterator m = core->getData()->mapIcons.find(mapiconid);
 	if (m != core->getData()->mapIcons.end())
 	{
-		return 1;
+		ret = true;
 	}
-	return 0;
+	API_RETURN(bool ret);
 }
